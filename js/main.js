02aa4f8f3a0030f9c70d144f9daf0b4d0bad199e
Michaël HEID <michael123456222444666@gmail.com>
2024-10-28 03:37:55 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -7,22 +7,21 @@ document.querySelector('.carre').addEventListener('click', function(){
     const infotext = document.getElementById('infotext');
 
 
-
 // innerHTML : Propriété qui permet de définir ou d'obtenir le contenu HTML d'un élément.
 
 
 // getComputedStyle : Méthode qui retourne un objet contenant les styles calculés d'un élément, ce qui permet d'obtenir les valeurs CSS en vigueur.
 
 
-
 // on récupère les propriétés CSS
 const backgroundColor = window.getComputedStyle(this).backgroundColor;
-const textColor = this.style.color; // Couleur du texte
+const textColor = window.getComputedStyle(this).color; // Couleur du texte
 const width = this.offsetWidth;
 const height = this.offsetHeight;
 const className = this.className; // Nom de la classe
 const fontSize = window.getComputedStyle(this).fontSize; // Taille de la police
-
+const display = window.getComputedStyle(this).display;
+const padding = window.getComputedStyle(this).padding;
 
 
 // Mise à jour du texte avec les propriétés
@@ -32,7 +31,9 @@ infotext.innerHTML = `
         Couleur du texte : ${textColor}<br>
         Largeur : ${width}px<br>
         Hauteur : ${height}px<br>
-        Taille de la police : ${fontSize}
+        Taille de la police : ${fontSize}<br>
+        Display : ${display}<br>
+        Padding : ${padding}
     `;
 
     rectangle.style.display = 'flex'; // Changer à 'flex' pour un affichage correct
